393ab967acafed92c684e4040304c925c5091c78
Alex Frankhouser <freesoul423@gmail.com>
2023-02-06 15:46:44 -0500

    debugged the sW

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,7 +15,7 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const jateDB = await openDB('jate', 1); //creates a connection the the db we want to use.
-  const tx = jateDB.transaction('contact', 'readwrite') //creates a new transaction and sets the privileges.
+  const tx = jateDB.transaction('jate', 'readwrite') //creates a new transaction and sets the privileges.
   const store = tx.objectStore('jate');
   const request = store.put({ id: 1, value: content });
   const result = await request;
@@ -27,7 +27,7 @@ export const getDb = async () => {
   const jateDB = await openDB('jate', 1); //Creates the connection to DB and says what version we will use.
   const tx = jateDB.transaction('jate', 'readonly'); //Creates a new transaction and specifies the database and data privileges.
   const store = tx.objectStore('jate'); //opens up the desired object store.
-  const request = store.getAll(); // Gets all the data from the database.
+  const request = store.getAll(1); // Gets all the data from the database.
   // Confirmation of the request
   const result = await request;
   console.log('result.value', result);
